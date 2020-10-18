3fffdc420219fb124c5cd545eb63725bda4c18de
Saloni Mohta <salonimohta37@gmail.com>
2020-10-18 16:23:14 +0530

    Adds login page

--- frontend/src/core/LoginPage/index.js
+++ frontend/src/core/LoginPage/index.js
@@ -1,46 +1,60 @@
 import React from 'react'
 import './index.css'
 import Login from '../../Components/Login'
-import {Tabs,Tab} from 'react-bootstrap'
 
 export default class LoginPage extends React.Component{
+   constructor(){
+     super();
+     this.state={
+       activeTab: "student"
+     };
+   }
+   handleSelect(type){
+     console.log(type);
+     this.setState({activeTab:type});
+   }
     render(){
         return(
             <div className="loginPage">
+              <div>
+                <br/>
                 <div className="loginBox">
+                  <div className="loginImage">
+                    <br/>
+                    <div className="loginHeader">Branch Change IIT(ISM)</div>
+                    <br/><br/>
+                    <img src={require("../../logo.png")} alt="ISM_logo" />
+                    </div>
+                    <div className="vl" />
+                  <div className="loginForm">
+                    <br/>
                 <div class="card-body">
                     <ul class="nav nav-pills" id="myTab3" role="tablist">
                       <li class="nav-item">
-                        <a class="nav-link" id="home-tab3" data-toggle="tab" href="#home3" role="tab" aria-controls="home" aria-selected="true">Home</a>
-                      </li>
+                        {this.state.activeTab==="student"?
+                        <a class="nav-link active" data-toggle="tab" href="#home3" role="tab" aria-controls="home" aria-selected="true" onClick={()=>this.handleSelect("student")}>Student</a>
+                        :
+                        <a class="nav-link" data-toggle="tab" href="#home3" role="tab" aria-controls="home" aria-selected="true" onClick={()=>this.handleSelect("student")}>Student</a>
+                        }
+                        </li>
                       <li class="nav-item">
-                        <a class="nav-link" id="profile-tab3" data-toggle="tab" href="#profile3" role="tab" aria-controls="profile" aria-selected="false">Profile</a>
+                        {this.state.activeTab==="admin"?
+                        <a class="nav-link active" data-toggle="tab" href="#profile3" role="tab" aria-controls="profile" aria-selected="false" onClick={()=>this.handleSelect("admin")}>Admin</a>
+                        :
+                        <a class="nav-link" data-toggle="tab" href="#profile3" role="tab" aria-controls="profile" aria-selected="false" onClick={()=>this.handleSelect("admin")}>Admin</a>
+                      }
                       </li>
                     </ul>
                     <div class="tab-content" id="myTabContent2">
-                      <div class="tab-pane fade active show" id="home3" role="tabpanel" aria-labelledby="home-tab3">
-                        <Login username="Admission No." type="Student" />
-                      </div>
-                      <div class="tab-pane fade" id="profile3" role="tabpanel" aria-labelledby="profile-tab3">
-                        <Login username="Email" type="Admin" />
-                      </div>
+                        {console.log(this.state.activeTab)}
+                        {this.state.activeTab==="student" ? <Login username="Admission No." type="Student" /> : <Login username="Email" type="Admin" />}
                     </div>
                   </div>
-                    {/*<div>
-                        <img src={require("../../logo.png")} alt="ISM_logo" />
+                  </div>
                     </div>
-                    <div className="loginForm">
-                    <Tabs defaultActiveKey="student" id="uncontrolled-tab-example">
-                    <Tab eventKey="student" title="Student">
-                      <Login username="Admission No." type="Student" />
-                    </Tab>
-                    <Tab eventKey="admin" title="Admin">
-                      <Login username="Email" type="Admin" />
-                    </Tab>
-                  </Tabs>
-                    </div>*/}
-        </div>
-            </div>
+                    <br/>
+                    </div>
+                    </div>
         )
     }
 }
