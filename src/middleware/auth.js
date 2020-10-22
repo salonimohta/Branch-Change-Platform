7a09b8f807ddbe018b47c794aee430f349959d09
ashutosh7599 <ashutosh7599@users.noreply.github.com>
2020-10-23 02:51:50 +0530

    auth was misdirecting. so commented it

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,21 +1,21 @@
 const jwt = require('jsonwebtoken')
-const User = require('../models/user')
+// const User = require('../models/user')
 
-const auth = async (req, res, next) => {
-    try{
-        const token = req.header('Authorization').replace('Bearer ','')
-        const decoded = jwt.verify(token, 'branchque')
-        const user = await User.findOne({_id: decoded._id, 'tokens.token':token})
-        if(!user){
-            throw new Error()
-        }
-        req.user = user
-        req.token = token
-        next()
-    } catch(e){
-        res.status(401).send({error: "Authentication required"})
-    }
-
-}
-
-module.exports = auth
+// const auth = async (req, res, next) => {
+//     try{
+//         const token = req.header('Authorization').replace('Bearer ','')
+//         const decoded = jwt.verify(token, 'branchque')
+//         const user = await User.findOne({_id: decoded._id, 'tokens.token':token})
+//         if(!user){
+//             throw new Error()
+//         }
+//         req.user = user
+//         req.token = token
+//         next()
+//     } catch(e){
+//         res.status(401).send({error: "Authentication required"})
+//     }
+//
+// }
+//
+// module.exports = auth
