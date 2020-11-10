9b80f7fa59a6474cb9389756a43b169516ece30f
Saloni Mohta <salonimohta37@gmail.com>
2020-11-10 20:16:12 +0530

    Adds comments in Layout Component

--- frontend/src/core/Layout/index.js
+++ frontend/src/core/Layout/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import Header from '../../Components/Header'
 import Footer from '../../Components/Footer'
 
+/* This Layout component specifies the layout of every page within the branch change platform, typical layout includes
+    1. Header
+    2. Page component (obtained from props)
+    3. Footer
+*/
+
 export default class Layout extends React.Component{
     constructor(){
         super();
