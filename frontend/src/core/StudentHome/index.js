b36050c769205a722fff9beecd062ad9188d7221
Saloni Mohta <salonimohta37@gmail.com>
2020-11-10 20:08:27 +0530

    Adds comments in Student Home Page

--- frontend/src/core/StudentHome/index.js
+++ frontend/src/core/StudentHome/index.js
@@ -5,12 +5,11 @@ import {API} from '../../config'
 import Session from 'react-session-api'
 import {viewRequestGeneratePDF} from './../../Components/viewRequestPDFGenerator'
 
+
 export default class StudentHome extends React.Component{
     constructor(){
         super();
         this.state={
-            errorMsg: '',
-            error: false,
             branchChangeRequestSubmitted: false,
             branchChangeApplication: []
         }
@@ -19,7 +18,10 @@ export default class StudentHome extends React.Component{
     requestBranchChange=()=>{
         this.props.history.push('/branchChangeRequest');
     }
+    /* Whenever the Student Home page is mounted, a get request is sent to the server to 
+    update the status about the branch change request submission of the student */
     componentDidMount(){
+        //Session token is set from localStorage because on refresh the Session gets cleared
         Session.set('token',localStorage.getItem('token'));
         axios({
             method: 'get',
@@ -31,7 +33,6 @@ export default class StudentHome extends React.Component{
              }
           })
           .then(response=>{
-              console.log(response);
               if (response.status===200){
               if (response.data.completeBranchChangeApplications.length>0){
                   this.setState({branchChangeRequestSubmitted: true,branchChangeApplication: response.data.completeBranchChangeApplications});
@@ -40,11 +41,11 @@ export default class StudentHome extends React.Component{
                     this.setState({branchChangeRequestSubmitted: false,branchChangeApplication: []});
                 }
             }
-            else alert('We could not fetch the branch application!');
           }) 
-          .catch(error => alert(error))
+          .catch(error => alert('The status of branch Change Request could not be fetched due to some error'))
     }
     render(){
+        /* details of the student are stored in constant variables to be used in the page */
         const imagePath=localStorage.getItem('imagePath');
         const name=localStorage.getItem('studentName');
         const admissionNo=localStorage.getItem('admissionNo');
@@ -65,6 +66,7 @@ export default class StudentHome extends React.Component{
                 </div>
                 </div>
             </div><br/><br></br>
+            {/* If the branch change request is submitted then, the student can only view the request as a pdf, and the request to fill the form will be disabled */}
             {this.state.branchChangeRequestSubmitted ? 
             <div className="buttonSpace">
             <div>
