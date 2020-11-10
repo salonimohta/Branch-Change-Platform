57b8ca887ec17a850749b6021720a5e8e809ba7d
Saloni Mohta <salonimohta37@gmail.com>
2020-11-11 01:51:40 +0530

    Adds comments in requiredData.js

--- frontend/src/requiredData.js
+++ frontend/src/requiredData.js
@@ -1,3 +1,17 @@
+/* 
+All the information about branch, course and department is used from this file, it is mostly used in BranchChangeRequestForm Component
+Branch/Course/Dept is hardcoded as of now.
+getKeyByValue is a function that gives a key for the corresponding value in a javascript object
+courseValueToIdMapping is a constant object containing the mapping from Value of course to id of course as present in db
+courseValueToNameMapping is a constant object containing the mapping from Value of course to Name of course as present in db
+deptByCourseCategory is a constant object containing the mapping of deptValues with Courses (self-assumed variable)
+deptValuesDropdown is a constant object containing the mapping of Course Values to an array of departments which belong to the course
+branchByCourseAndDeptCategory is a constant object of objects where each course has been mapped to objects of departments it belongs to which is further mapped to an array of branches which fall into the respective departent
+branchValuesDropdown is a similar structure like branchByCourseAndDeptCategory, except instead of names of branches, ids of branches are used
+branchNameValueMapping is a constant object containing the mapping of branch values used in code to branch names as present in db
+deptNameValueMapping is a constant object containing the mapping of dept values used in code to dept names as present in db
+*/
+
 export function getKeyByValue(object, value) { 
     for (var prop in object) { 
         if (object.hasOwnProperty(prop)) { 
