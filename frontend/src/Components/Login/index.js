e4e36983eda1da4e606a6876ce4540beb1f5f8f5
Saloni Mohta <salonimohta37@gmail.com>
2020-11-10 23:46:59 +0530

    Removes index.css from login component

--- frontend/src/Components/Login/index.js
+++ frontend/src/Components/Login/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import './index.css'
-import {Redirect, withRouter} from 'react-router-dom'
+import {withRouter} from 'react-router-dom'
 import axios from "axios"
 import {API} from '../../config'
 import Session from 'react-session-api'
