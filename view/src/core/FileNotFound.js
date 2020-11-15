0ef5829a348dba63b3ba9aa40c2be80ec2896288
Saloni Mohta <salonimohta37@gmail.com>
2020-11-15 16:19:54 +0530

    Removes dummy files

--- view/src/core/FileNotFound.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-const FileNotFound=()=>(
-  <div>
-  <h3>File not found</h3>
-  </div>
-)
-export default FileNotFound;
--- view/src/helpers/index.js
+++
@@ -1,3 +0,0 @@
-const getIsLoggedIn = () => localStorage.getItem('misData') === 'false';
-
-export default getIsLoggedIn;
